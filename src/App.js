4b646ff34b7057a7cd9de42c349b7becb0bb9768
Federico Farias <federico.farias@outlook.com>
2024-02-22 15:22:53 -0600

    Se integra bootstrap 5

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import { Link, Outlet } from 'react-router-dom';
+//import { Link, Outlet } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Nav } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <nav>
-          <ul>
-            <li>
-              <Link to={`/`}>Home</Link>
-            </li>
-            <li>
-              <Link to={`/classroom-reservations`}>Reservación de aulas</Link>
-            </li>
-          </ul>
-        </nav>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Outlet />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className='App2'>
+      <Row>
+        <Col md="2">
+          <Nav defaultActiveKey="/home" className="flex-column">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href='/classroom-reservations' eventKey="link-1">Reservación de aulas</Nav.Link>
+          </Nav>
+        </Col>
+        <Col md="10" style={{ backgroundColor: 'white'}}>
+          <Outlet />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/pages/classroom-reservation-page.js
+++ src/pages/classroom-reservation-page.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { useRouteError } from "react-router-dom";
 
 export default function ClassRoomReservationPage() {
-  const error = useRouteError();
-  console.error(error);
-
   return (
-    <div id="error-page">
+    <div>
       <h1>Reservación de aulas</h1>
       <p>Reservación de aulas</p>
     </div>
